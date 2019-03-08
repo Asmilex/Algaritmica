487f500c2b3509e56ae4d416bc715dadd7d76864
Mapachana <anabuenrua99@gmail.com>
2019-03-08 19:15:11 +0100

    Modificado el script de pivotajo para los valores correspondientes de burbuja

--- Documentación/codigo/script_pivotajo.sh
+++ Documentación/codigo/script_pivotajo.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-for j in {1..13}; do
+for j in {1..10}; do
     N=100
 
-    for i in {1..4}; do
+    for i in {1..24}; do
         ./pivotajo $N >> resultados.txt
-        N=$(($N*10))
+        N=$(($N+20000))
     done
 done
 
 #./pivotajo 500000 >> resultados.txt
+#./pivotajo 700000 >> resultados.txt
 #./pivotajo 1000000 >> resultados.txt
