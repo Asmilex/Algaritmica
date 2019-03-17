231ece20df96b2f577b61ed381426134d283bb4e
Mapachana <anabuenrua99@gmail.com>
2019-03-17 16:28:00 +0100

    Añado script mergesort

--- Documentación/codigo/script_mergesort.sh
+++ Documentación/codigo/script_mergesort.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-for i in {1..15}; do
-    touch resultados_hanoi_temp.dat
-    ./mergesort resultados_hanoi_temp.dat 100 800 1500 2200 2900 3600 4300 5000 5700 6400 7100 7800 8500 9200 9900 10600 11300 12000 12700 13400 14100 14800 15500 16200 16900 17600 18300 19000
-done
+N=100
 
-sort -n resultados_hanoi_temp.dat >> resultados_hanoi.dat
-rm resultados_hanoi_temp.dat
+for j in {1..28}; do
+
+    for i in {1..15}; do
+	
+        ./mergesort resultados_mergesort.dat `date +%s` $N
+        
+    done
+
+    N=$(($N+700))
+
+done
