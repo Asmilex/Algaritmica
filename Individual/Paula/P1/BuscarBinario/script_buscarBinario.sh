5aedfcf47d605ed9b6e05956e84fd364a05478ea
Mapachana <anabuenrua99@gmail.com>
2019-03-17 19:11:24 +0100

    QUE QUIERO QUE AJUSTE A MALDITA SEA

--- Individual/Paula/P1/BuscarBinario/script_buscarBinario.sh
+++ Individual/Paula/P1/BuscarBinario/script_buscarBinario.sh
@@ -4,12 +4,12 @@ N=100
 
 for j in {1..28}; do
 
-    for i in {1..100}; do
+    for i in {1..150}; do
 	
         ./BuscarBinario $N >> resultados_buscarBinario.dat
         
     done
 
-    N=$(($N+70000))
+    N=$(($N+50000))
 
 done
