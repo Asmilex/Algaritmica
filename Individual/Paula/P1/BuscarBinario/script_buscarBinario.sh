331fd60f73d68428210057377c93e52895e0eb04
paula1999 <pvillanunez@correo.ugr.es>
2019-03-17 18:37:52 +0100

    añado buscarbinario

--- Individual/Paula/P1/BuscarBinario/script_buscarBinario.sh
+++ Individual/Paula/P1/BuscarBinario/script_buscarBinario.sh
@@ -4,12 +4,12 @@ N=100
 
 for j in {1..28}; do
 
-    for i in {1..25}; do
+    for i in {1..40}; do
 	
         ./BuscarBinario $N >> resultados_buscarBinario.dat
         
     done
 
-    N=$(($N+75000))
+    N=$(($N+70000))
 
 done
