33c595431c604ba4f9ba591dc7a05403e397aad2
Asmilex <andresmm@outlook.com>
2019-03-28 12:07:21 +0100

    Modificada traspuesta según PDF

--- P2/Programas/script_traspuesta.sh
+++ P2/Programas/script_traspuesta.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 N=100
+filas=10
+columnas=10
 
 for i in {1..50} do
-	./traspuesta $N
+	./traspuesta $filas $columnas $N
 	N=$(($N+700)) >> traspuesta.log
 done
--- P2/Programas/traspuesta.cpp
+++ P2/Programas/traspuesta.cpp
@@ -4,32 +4,32 @@
 
 using namespace std;
 
-void generar_aleatorios (vector<vector<int>> &peaso_matriz, const size_t tamano) {
+void generar_aleatorios (vector<vector<int>> &peaso_matriz, const size_t N, const size_t M) {
     std::mt19937 rng;
     rng.seed(std::random_device()());
     std::uniform_int_distribution<std::mt19937::result_type> dist(-10000,10000);
 
-    for (size_t i = 0; i < tamano; ++i)
-        for (size_t j = 0; j < tamano; ++j)
+    for (size_t i = 0; i < N; ++i)
+        for (size_t j = 0; j < M; ++j)
             peaso_matriz[i][j] = dist(rng);
 
 }
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 2) {
-        cerr << "Fallo en los parámetros";
+    if (argc != 4) {
+        cerr << "./traspuesta filas columnas tamaño. Matriz de N \\cdot M";
         return -1;
     }
 
-    int tamano = atoi(argv[1]);
+    int N = atoi(argv[1]), M = atoi(argv[2]);
 
-    vector<vector<int>> matriz (tamano, vector<int>(tamano));
+    vector<vector<int>> matriz (N, vector<int>(M));
 
-    generar_aleatorios(matriz, tamano);
+    generar_aleatorios(matriz, N, M);
 
-    for (size_t i = 0; i < tamano; ++i) {
-        for (size_t j = 0; j < tamano; ++j)
+    for (size_t i = 0; i < N; ++i) {
+        for (size_t j = 0; j < M; ++j)
             cout << matriz[i][j] << "\t";
 
         cout << "\n";
