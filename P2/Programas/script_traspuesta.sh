62047c1ef65ecfd2ce4fb90f2d011f1102374a2a
Asmilex <andresmm@outlook.com>
2019-03-28 12:18:11 +0100

    Modificados scripts, añadido clean_log.sh

--- P2/Programas/script_traspuesta.sh
+++ P2/Programas/script_traspuesta.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+mv traspuesta.log traspuesta_old.log
+
+make clean
+make
 
 N=100
 filas=10
 columnas=10
 
-for i in {1..50} do
-	./traspuesta $filas $columnas $N
-	N=$(($N+700)) >> traspuesta.log
+for i in {1..50}; do
+	./traspuesta $filas $columnas $N >> traspuesta.log
+	N=$(($N+700))
 done
--- P2/Programas/script_vector.sh
+++ P2/Programas/script_vector.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-N=100
+mv vector.log vector_old.log
+make clean
+make
 
-for i in {1..50} do
-	./vector $N
-	N=$(($N+700)) >> vector.log
+N=1000
+
+for i in {1..50}; do
+	./vector $N >> vector.log
+	#N=$(($N+700))
 done
