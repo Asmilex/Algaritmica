4207e8e6afa8425d32ef706a08dd7e06401f9e73
Mapachana <anabuenrua99@gmail.com>
2019-04-03 18:30:56 +0200

    Modifico script para tener varias ejecutaciones por valor

--- P2/Programas/script_traspuesta.sh
+++ P2/Programas/script_traspuesta.sh
@@ -9,6 +9,8 @@ filas=10
 columnas=10
 
 for i in {1..50}; do
-	./traspuesta $filas $columnas $N >> traspuesta.log
-	N=$(($N+700))
+    for j in {1..3}; do
+	    ./traspuesta $filas $columnas $N >> traspuesta.log
+    done	
+    N=$(($N+700))
 done
