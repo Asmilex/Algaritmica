14dcf4ac320918ee7b6284b7061fb0c6bf0b9cab
Mapachana <anabuenrua99@gmail.com>
2019-04-09 17:29:27 +0200

    Arreglo conflictos y ahora si que rezo

--- P2/graficas/vector.cpp
+++ P2/graficas/vector.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <vector>
 #include <iostream>
 #include <random>
@@ -6,16 +5,9 @@
 
 using namespace std;
 
-int maximo(const vector<int> & flechita) {
-    return * max_element(flechita.begin(), flechita.end());
-}
+int maximo (vector<int> &flechita, int l, int r) {
 
-int minimo(const vector<int> & flechita) {
-    return * min_element(flechita.begin(), flechita.end());
-}
-
-int maximoDyV (const vector<int> &flechita, int l, int r) {
-    if(l<=r) {
+    if(l<=r){
         if (r - l <= 1) {
             if (flechita[l] < flechita[r])
                 return flechita[r];
@@ -24,29 +16,32 @@ int maximoDyV (const vector<int> &flechita, int l, int r) {
         }
         else {
             int m    = (l + r)/2;
-            int maxL = maximoDyV(flechita, l, m);
-            int maxR = maximoDyV(flechita, m + 1, r);
+            int maxL = maximo(flechita, l, m);
+            int maxR = maximo(flechita, m + 1, r);
 
             if (maxL < maxR)
                 return maxR;
             else
                 return maxL;
-        }
+        }        
     }
+
 }
 
-int minimoDyV (const vector<int> &flechita, int l, int r) {
-    if (l<=r) {
+int minimo (vector<int> &flechita, int l, int r) {
+
+    if(l<=r){
         if (r - l <= 1) {
             if (flechita[l] > flechita[r])
                 return flechita[r];
             else
                 return flechita[l];
         }
+
         else {
             int m    = (l + r)/2;
-            int minL = minimoDyV(flechita, l, m);
-            int minR = minimoDyV(flechita, m + 1, r);
+            int minL = minimo(flechita, l, m);
+            int minR = minimo(flechita, m + 1, r);
 
             if (minL < minR)
                 return minL;
@@ -67,13 +62,15 @@ void generar_aleatorios (vector<int> &flechita, const size_t tamano) {
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 3) {
+    if (argc != 2) {
         cerr << "Fallo en los parámetros";
         return -1;
     }
+     chrono::time_point<std::chrono::high_resolution_clock> t0, tf; // Para medir el tiempo de ejecución
+	double tejecucion; // tiempo de ejecucion del algoritmo en ms
 
     int n = atoi(argv[1]);
-    bool is_DyV = atoi(argv[2]);
+
 
     vector<int> flechita;
 
@@ -81,22 +78,13 @@ int main(int argc, char const *argv[])
 
     /*for (auto num: flechita)
         cout << num << ", ";*/
-
-    chrono::time_point<std::chrono::high_resolution_clock> t0, tf; // Para medir el tiempo de ejecución
     chrono::high_resolution_clock::time_point t_antes = chrono::high_resolution_clock::now();
-
-    if (is_DyV) {
-        int maximovector = maximoDyV(flechita, 0, flechita.size());
-        int minimovector = minimoDyV(flechita, 0, flechita.size());
-    }
-    else {
-        int maximovector = maximo(flechita);
-        int minimovector = minimo(flechita);
-    }
-
+    int maximovector = maximo(flechita, 0, flechita.size());
+    int minimovector = minimo(flechita, 0, flechita.size());
     chrono::high_resolution_clock::time_point t_despues = chrono::high_resolution_clock::now();
   	unsigned long t_ejecucion = chrono::duration_cast<std::chrono::microseconds>(t_despues - t_antes).count();
-  	cout << "para tamanio: " << n << " tiempo de ejecucion: "<< t_ejecucion <<endl;
+  	cout << " " << n << "   "<< t_ejecucion <<endl;
     //cout << "\nMáximo: " << maximo(flechita, 0, flechita.size() - 1) << "\n\n";
     //cout << "\nMínimo: " << minimo(flechita, 0, flechita.size() - 1) << "\n\n";
+
 }
